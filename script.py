91f492fcd3d9e51ca89b605c323059fc2c588796
gbonez <gruehlmann2@gmail.com>
2025-10-29 20:14:08 -0400

    minor print line change to notify when script starts

--- script.py
+++ script.py
@@ -8,6 +8,9 @@ from spotipy.oauth2 import SpotifyOAuth
 from spotipy.exceptions import SpotifyException
 
 # ==== CONFIGURATION ====
+print(f"Starting Release Radar Script..")
+time.sleep(10)
+
 SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.environ.get("BASE_URL") + "/callback"
